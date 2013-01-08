01c0edaced49587711927e0259b9842ca7e07d62
James Schnable <jschnable@nope.local>
2013-01-08 17:41:44 -0600

    Results was still pointing to a legacy CSS file

--- web_interface/results.php
+++ web_interface/results.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-<link rel="stylesheet" type="text/css" media="all" href="/qteller2/css/common.css" />
+<link rel="stylesheet" type="text/css" media="all" href="css/common.css" />
 <div id="mainContainer">
 <?php require('header.html'); ?>
 <div id="pageContents">
